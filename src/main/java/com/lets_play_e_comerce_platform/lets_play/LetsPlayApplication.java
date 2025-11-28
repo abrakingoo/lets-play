53d3bdad65abf9e94340672dba0255e4bc863254
abrakingoo <maikoabraham68@gmail.com>
2025-11-28 18:16:22 +0300

    Remove invalid MongoDB configuration from constructor

--- src/main/java/com/lets_play_e_comerce_platform/lets_play/LetsPlayApplication.java
+++ src/main/java/com/lets_play_e_comerce_platform/lets_play/LetsPlayApplication.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LetsPlayApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(LetsPlayApplication.class, args);
 	}
