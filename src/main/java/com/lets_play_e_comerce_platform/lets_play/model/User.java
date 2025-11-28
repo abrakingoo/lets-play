570fc645581dbb4a95c2ec121d19735492b5f0b4
abrakingoo <maikoabraham68@gmail.com>
2025-11-29 00:20:58 +0300

    Add default constructor and MongoDB annotations to User model

--- src/main/java/com/lets_play_e_comerce_platform/lets_play/model/User.java
+++ src/main/java/com/lets_play_e_comerce_platform/lets_play/model/User.java
@@ -2,12 +2,20 @@ package com.lets_play_e_comerce_platform.lets_play.model;
 
 import java.util.UUID;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
 public class User {
+    @Id
     String id;
+
     String name;
     String email;
     String password;
     String role;
+
+    // public User() {}
 
     public User(String name, String email, String password, String role) {
         this.id = UUID.randomUUID().toString();
@@ -22,4 +30,9 @@ public class User {
     public String getEmail() { return email; }
     public String getPassword() { return password; }
     public String getRole() { return role; }
+
+    public void setName(String name) { this.name = name; }
+    public void setEmail(String email) { this.email = email; }
+    public void setPassword(String password) { this.password = password; }
+    public void setRole(String role) { this.role = role; }
 }
