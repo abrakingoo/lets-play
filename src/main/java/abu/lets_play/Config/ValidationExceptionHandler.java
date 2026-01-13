47633b0a5c3cc6933c896351e7c676bc74852b22
abrakingoo <maikoabraham68@gmail.com>
2026-01-13 14:51:01 +0300

    Add configuration classes

---
+++ src/main/java/abu/lets_play/Config/ValidationExceptionHandler.java
@@ -0,0 +1,28 @@
+package abu.lets_play.Config;
+
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ValidationExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, Object> handleValidationErrors(MethodArgumentNotValidException ex) {
+
+        Map<String, String> fieldErrors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            fieldErrors.put(error.getField(), error.getDefaultMessage());
+        });
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", 400);
+        response.put("errors", fieldErrors);
+
+        return response;
+    }
+}
