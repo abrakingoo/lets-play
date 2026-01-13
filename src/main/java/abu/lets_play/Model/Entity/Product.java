37e5fd54b4d64c2a271be840e85a3c5e67ab9858
abrakingoo <maikoabraham68@gmail.com>
2026-01-13 14:48:40 +0300

    Create Product entity for MongoDB

---
+++ src/main/java/abu/lets_play/Model/Entity/Product.java
@@ -0,0 +1,56 @@
+package abu.lets_play.Model.Entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "products")
+public class Product {
+    @Id
+    private String id;
+
+    private String name;
+    private String description;
+    private int price;
+
+    public Product() {
+    }
+
+    public Product(String name, String description, Integer price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+}
