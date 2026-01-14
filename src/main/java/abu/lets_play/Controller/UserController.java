e0dd4330d9a73adf5d1ec36fa239c6af323359a0
abrakingoo <maikoabraham68@gmail.com>
2026-01-14 13:47:45 +0300

    Remove auth endpoints from UserController

--- src/main/java/abu/lets_play/Controller/UserController.java
+++ src/main/java/abu/lets_play/Controller/UserController.java
@@ -4,14 +4,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import abu.lets_play.Model.Entity.User;
-import abu.lets_play.Model.dto.UserSignIn;
-import abu.lets_play.Model.dto.UserSignUp;
 import abu.lets_play.Service.Userservice;
-import jakarta.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -26,19 +21,9 @@ public class UserController {
         this.userService = userService;
     }
 
-    @PostMapping("/signup")
-    public ResponseEntity<?> signUp(@Valid @RequestBody UserSignUp entity) {
-        return userService.signUp(entity);
-    }
-
     @GetMapping
     public List<User> getMethodName() {
         return userService.findAll();
-    }
-
-    @PostMapping("/signin")
-    public ResponseEntity<?> signIn(@Valid @RequestBody UserSignIn entity) {
-        return userService.signIn(entity); 
     }
 
     @GetMapping("/{id}")
