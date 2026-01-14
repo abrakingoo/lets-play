c82f017b0edd4ec002cc79a08dcbbd5bd386104b
abrakingoo <maikoabraham68@gmail.com>
2026-01-14 11:44:21 +0300

    update naming typo

--- src/main/java/abu/lets_play/Config/SecurityConfig.java
+++ src/main/java/abu/lets_play/Config/SecurityConfig.java
@@ -7,7 +7,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityCinfig {
+public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
