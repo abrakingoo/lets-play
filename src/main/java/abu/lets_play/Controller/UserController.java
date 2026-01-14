8602ababdf07e8ed431a6acc55265baa22820096
abrakingoo <maikoabraham68@gmail.com>
2026-01-14 07:38:09 +0300

    Refactor UserController to use service layer

---
+++ src/main/java/abu/lets_play/Controller/UserController.java
@@ -0,0 +1,39 @@
+package abu.lets_play.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import abu.lets_play.Model.Entity.User;
+import abu.lets_play.Model.dto.UserSignUp;
+import abu.lets_play.Service.Userservice;
+import jakarta.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import java.util.Map;
+
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final Userservice userService;
+
+    public UserController(Userservice userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signUp(@Valid @RequestBody UserSignUp entity) {
+        try {
+            User savedUser = userService.createUser(entity);
+            return ResponseEntity.status(HttpStatus.CREATED).body(Map.of(
+                "message", "User created successfully",
+                "user", savedUser
+            ));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("error", e.getMessage()));
+        }
+    }
+}
