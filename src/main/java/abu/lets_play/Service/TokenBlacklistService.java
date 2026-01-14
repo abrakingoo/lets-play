06a17a1dad886525badfbb6af3da49986db8508f
abrakingoo <maikoabraham68@gmail.com>
2026-01-14 15:02:31 +0300

    feat: add token blacklist service for logout functionality

---
+++ src/main/java/abu/lets_play/Service/TokenBlacklistService.java
@@ -0,0 +1,19 @@
+package abu.lets_play.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TokenBlacklistService {
+    private final Set<String> blacklistedTokens = new HashSet<>();
+
+    public void blacklistToken(String token) {
+        blacklistedTokens.add(token);
+    }
+
+    public boolean isBlacklisted(String token) {
+        return blacklistedTokens.contains(token);
+    }
+}
