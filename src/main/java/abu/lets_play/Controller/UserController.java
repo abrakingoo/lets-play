443d3f3dccc9e2ab136ef136da7c290ec527ce93
abrakingoo <maikoabraham68@gmail.com>
2026-01-14 11:41:12 +0300

    Add input validation to UserController endpoints

--- src/main/java/abu/lets_play/Controller/UserController.java
+++ src/main/java/abu/lets_play/Controller/UserController.java
@@ -8,16 +8,12 @@ import abu.lets_play.Model.dto.UserSignUp;
 import abu.lets_play.Service.Userservice;
 import jakarta.validation.Valid;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
-import java.util.List;
-import java.util.Map;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -30,20 +26,11 @@ public class UserController {
 
     @PostMapping("/signup")
     public ResponseEntity<?> signUp(@Valid @RequestBody UserSignUp entity) {
-        try {
-            User savedUser = userService.createUser(entity);
-            return ResponseEntity.status(HttpStatus.CREATED).body(Map.of(
-                "message", "User created successfully",
-                "user", savedUser
-            ));
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("error", e.getMessage()));
-        }
+        return userService.signUp(entity);
     }
 
     @GetMapping
     public List<User> getMethodName() {
         return userService.findAll();
     }
-    
 }
