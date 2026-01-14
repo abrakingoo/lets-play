56548b4202bbd05857e2d2772dfbfd3e5de9eb0f
abrakingoo <maikoabraham68@gmail.com>
2026-01-14 12:25:19 +0300

    Update UserController to use UserSignIn DTO

--- src/main/java/abu/lets_play/Controller/UserController.java
+++ src/main/java/abu/lets_play/Controller/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import abu.lets_play.Model.Entity.User;
+import abu.lets_play.Model.dto.UserSignIn;
 import abu.lets_play.Model.dto.UserSignUp;
 import abu.lets_play.Service.Userservice;
 import jakarta.validation.Valid;
@@ -12,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -33,4 +35,16 @@ public class UserController {
     public List<User> getMethodName() {
         return userService.findAll();
     }
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> signIn(@Valid @RequestBody UserSignIn entity) {
+        return userService.signIn(entity); 
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findUserById(@PathVariable String id) {
+        return userService.findById(id);
+    }
+    
+    
 }
