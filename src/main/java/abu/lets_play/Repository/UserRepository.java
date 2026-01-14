50405b404a728eee6a32f60dde0510e51849d681
abrakingoo <maikoabraham68@gmail.com>
2026-01-14 11:05:12 +0300

    Update UserRepository to return Optional for findByEmail

--- src/main/java/abu/lets_play/Repository/UserRepository.java
+++ src/main/java/abu/lets_play/Repository/UserRepository.java
@@ -1,9 +1,11 @@
 package abu.lets_play.Repository;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import abu.lets_play.Model.Entity.User;
 
 public interface UserRepository extends MongoRepository<User, String>{
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 } 
