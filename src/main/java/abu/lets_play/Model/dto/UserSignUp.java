ddb0e4bff6b6235e3b91902ba7ea78f1bcb716cd
abrakingoo <maikoabraham68@gmail.com>
2026-01-14 07:37:47 +0300

    Add UserSignUp DTO for user registration

---
+++ src/main/java/abu/lets_play/Model/dto/UserSignUp.java
@@ -0,0 +1,34 @@
+package abu.lets_play.Model.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class UserSignUp {
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    @NotBlank(message = "Role is required")
+    private String role;
+
+    public UserSignUp(){}
+
+    public UserSignUp(String name, String email, String password, String role) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getName() {return name; }
+    public String getEmail() {return email; }
+    public String getPassword() {return password; }
+    public String getRole() {return role;}
+
+    public  void setName(String name) {this.name = name;}
+}
